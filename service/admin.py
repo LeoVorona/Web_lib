9b26d000d7c16756c0a47dd73de25e21a209bc3b
leoflex <voronaleonid@mail.ru>
2022-07-24 23:18:50 +0300

    lesson_9

--- service/admin.py
+++ service/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 
-from .models import Product, Bin
+from .models import Product, Bin, Home
 
 #admin.site.register(Product)
 admin.site.register(Bin)
+
 @admin.register(Product)
 class ProductAdmim(admin.ModelAdmin):
-    list_display = ['id', 'cost', 'title']
+    list_display = ['id', 'cost', 'title', 'is_active']
     fields = ['id', 'title']
     list_filter = ['cost']
     search_fields = ['title']
 
 #admin.site.register(Product, ProductAdmim)
+
+@admin.register(Home)
+class HomeAdmim(admin.ModelAdmin):
+    list_display = ['size', 'cost', 'adres', 'bal']
--- service/models.py
+++ service/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from turtle import title
 from django.db import models
 
@@ -7,6 +8,7 @@ class Product(models.Model):
     id = models.IntegerField(primary_key=True)
     cost = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200, null=True)
+    is_active = models.BooleanField(verbose_name="Активен", default=True ,null=True)
 
     def __str__(self):
         return self.title
@@ -16,6 +18,17 @@ class Bin(models.Model):
     id = models.IntegerField(primary_key=True)
     cost = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200, null=True)
+    
 
     def __str__(self):
         return self.title
+
+class Home(models.Model):
+
+    size = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Размер", null=True)
+    cost = models.IntegerField(verbose_name="Цена",null=True )
+    adres = models.CharField(max_length=200, verbose_name="Адрес", default="Армавирская")
+    bal = models.BooleanField(verbose_name="Балкон", default=True)
+
+    def __srt__(self):
+        return self.adres
