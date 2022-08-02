d9f979c9da9c80dd0a23d5e484cc5a10ab37f9f5
leoflex <voronaleonid@mail.ru>
2022-08-03 02:50:11 +0300

    less_17

--- web_lib/urls.py
+++ web_lib/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from web_lib.views import main, authors, books, about
+from web_lib.views import main, authors, author_id, books, book_id, about
 
 urlpatterns = [
     path('', main, name = 'web_lib'),
     path('authors', authors, name = 'authors'),
+    path('author/<uuid:pk>', author_id, name = 'author_id'),
     path('books', books, name = 'books'),
+    path('book/<int:pk>', book_id, name = 'book_id'),
     path('about', about, name = 'about'),
 ]
--- web_lib/views.py
+++ web_lib/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from web_lib.models import Author, Book
+from django.db.models import Count
 
 def main(request):
     return render(request, 'web_lib/main.html')
@@ -8,9 +9,21 @@ def authors(request):
     all_authors = {'authors': Author.objects.all()}
     return render(request, 'web_lib/authors.html', all_authors)
 
+def author_id(request, pk):
+    author = Author.objects.get(pk=pk)
+    books_amount = author.book_set.count()
+    founded_author = {'author': author, 'books_amount' : books_amount }
+    return render(request, 'web_lib/author_id.html', founded_author)   
+
 def books(request):
     all_books = {'books': Book.objects.all()}
     return render(request, 'web_lib/books.html', all_books)
+
+def book_id(request, pk):
+    book = Book.objects.get(pk=pk)
+    founded_book = {'book': book}
+    return render(request, 'web_lib/book_id.html', founded_book)       
+
     
 def about(request):
     return render(request, 'web_lib/about.html')
